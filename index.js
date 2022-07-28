25e9bb296852efb0c7bf5b0d99f221acebfd7de6
Tomas <tomas.pandu@gmail.com>
2022-07-28 00:25:45 -0300

    asd

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 import express from "express";
-import { PORT } from "./config.js";
+import { PORT } from "./puerto.js";
 import cors from "cors";
 import indexRoutes from "./routes/index.routes.js";
 import taskRoutes from "./routes/tasks.routes.js";
-import sequelize from "./conexions.js";
+import config from './config/config.js'
 import './models/tasks.js';
 const app = express();
-
+app.use(config);
 app.use(express.json());
 app.use(cors());
 app.use(indexRoutes);
